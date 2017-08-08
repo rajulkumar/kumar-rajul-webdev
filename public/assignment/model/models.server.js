748e23c3aa37e667fc7de9070b2f1534fdf46e86
rajulkumar <kumar.raju@husky.neu.edu>
2017-08-08 01:08:34 -0400

    schemas

---
+++ public/assignment/model/models.server.js
@@ -0,0 +1,9 @@
+var q=require('q');
+
+var connectionString='mongodb://127.0.0.1:27017/assignment';
+
+var mongoose=require('mongoose');
+var db=mongoose.connect(connectionString);
+
+module.exports=db;
+
